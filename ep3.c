32405897b55bb96c75c73cacb835b98dcd73a916
Buenobarbie <barbarabueno140104@gmail.com>
2022-11-07 13:54:00 -0300

    dinamic freq

--- ep3.c
+++ ep3.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "dataStructures.c"
 
-celula * frequencias[100];
 
 int main(){
     //Armazenando as palavras e o tamanho
@@ -12,6 +11,7 @@ int main(){
     int sizePalavra = 0;
     int i = 0, j = 0;
     int k = 1;
+    int maxFreq = 1;
     char *palavra;
     node * raiz;
     node * aux;
@@ -31,9 +31,11 @@ int main(){
                 aux = busca_arvore(raiz, palavra);
                 if(aux == NULL)
                     raiz = insere(raiz, palavra, sizePalavra);
-                else
+                else{
                     aux->freq++;
-                
+                    if(aux->freq > maxFreq)
+                        maxFreq = aux->freq;
+                }
             }
             iniPalavra = i+1;
             sizePalavra = 0;
@@ -42,9 +44,11 @@ int main(){
             sizePalavra++;
         i++;
     }
+    celula ** frequencias = malloc(sizeof(celula *) * (maxFreq + 1));
+
     inordem(raiz, frequencias);
     while(k != 0){
-    printf("Frequencias :");
+    printf("Digite um k (menor  ou igual a %d):", maxFreq + 1);
     scanf("%d", &k);
     imprimeListaRec(frequencias[k]);
 
