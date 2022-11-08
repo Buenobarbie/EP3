2897f718a152550c1dd9c45ca91681191e1f12e3
Buenobarbie <barbarabueno140104@gmail.com>
2022-11-08 06:06:44 -0300

    Adding comments

--- dataStructures.c
+++ dataStructures.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define item char *
+
+//Definicoes para a funcao de comparacao
 #define igual 1
 #define maior 2
 #define menor 3
 
-//Binary Search Tree setup
+//Binary Search Tree structure
 typedef struct cel_arvore {
   item info; 
   int size;
@@ -14,13 +16,12 @@ typedef struct cel_arvore {
   struct cel_arvore * dir;
 } node; 
 
-
-
-//Linked list setup
+//Linked list structure
 typedef struct cel{
     item info;
     struct cel *prox;
 } celula;
+
 
 int comparaPalavras(char * palavra1, char * palavra2);
 node * busca_arvore (node * raiz, item x);
@@ -34,6 +35,7 @@ void imprimePalavra(char * palavra);
 //Binary Search Tree functions
 
 int comparaPalavras(char * palavra1, char * palavra2){
+  //Compara , de acordo com a ordem alfabetica , duas palavras
   int i = 0;
   while(palavra1[i] && palavra2[i]){
     if(palavra1[i] > palavra2[i])
@@ -50,6 +52,9 @@ int comparaPalavras(char * palavra1, char * palavra2){
 }
 
 node * busca_arvore (node * raiz, item x){
+  //Verifica se o item x ja existe na arvore
+  //em caso positivo , o retorna 
+  //em caso negativo , retorna NULL
   if (raiz == NULL) return NULL;
   if (comparaPalavras(raiz->info, x) == igual) return raiz; 
   if (comparaPalavras(raiz->info, x) == menor)
@@ -58,6 +63,7 @@ node * busca_arvore (node * raiz, item x){
 }
 
 node * insere (node * raiz, item x, int size){
+  //Insere , em ordem alfabetia, a palavra na arvore
   if (raiz == NULL){
     raiz = malloc(sizeof (node));
     raiz->info = x;
@@ -76,9 +82,10 @@ node * insere (node * raiz, item x, int size){
 }
 
 void inordem(node * raiz, celula * freq[100]){
+  //Passa por todos os nodes da arvore em ordem alfabetica
   if (raiz != NULL){
     inordem(raiz->esq,freq);
-    imprimePalavra(raiz->info);
+    //imprimePalavra(raiz->info);
     freq[raiz->freq] = insereNoFim(freq[raiz->freq], raiz->info);
     inordem(raiz->dir,freq);
   }
@@ -88,6 +95,7 @@ void inordem(node * raiz, celula * freq[100]){
 //Linked List functions
 
 celula * insereNoFim(celula * inicio, item x){
+    //Adiciona o elemento no fim da lista ligada
     celula * aux = malloc(sizeof(celula));
     aux->info = x;
     aux->prox = NULL;
@@ -101,6 +109,7 @@ celula * insereNoFim(celula * inicio, item x){
 }
 
 void imprimeListaRec(celula * inicio){
+  //Imprime os elementos da lista ligada
     if(inicio != NULL){
         imprimePalavra(inicio->info);
         imprimeListaRec(inicio->prox);
@@ -110,6 +119,7 @@ void imprimeListaRec(celula * inicio){
 }
 
 void imprimePalavra(char * palavra){
+  //Imprime uma palavra
     for(int i = 0; palavra[i]; i++)
         printf("%c", palavra[i]);
     printf("\n");
--- ep3.c
+++ ep3.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dataStructures.c"
-
+#include <string.h>
 
 int main(){
-    //Armazenando as palavras e o tamanho
     FILE * fptr;
     fptr = fopen("example.txt","r");
     char c = 'a';
@@ -18,38 +17,74 @@ int main(){
     node * aux;
 
     while(c != EOF){
+        /*Pegando o caracter do arquivo */
         c = getc(fptr);
+
+        /*Passando todas as letras maiusculas para minusculas */
         if('A'<= c && c <= 'Z')
             c += 32;
+        
+        /*Se o caracter não for uma letra, entao chegou ao fim de uma palavra */
         if(!('a' <= c && c <= 'z')){
+            /*So sera realmente o fim de uma palavra se o caracter anterior for uma letra,
+            ou seja ,se o tamanho da palavra nao for zero */
             if(sizePalavra!= 0){
+                /*Armazenar a palavra */
                 palavra = malloc(sizeof(char) * sizePalavra + 1);
                 for(j = 0; j < sizePalavra; j++){
                     palavra[j] = auxPalavra[j];
                 }
                 palavra[j] = '\0';
+
+                /*verificar se a palavra ja esta na arvore */
                 aux = busca_arvore(raiz, palavra);
+
+                /*Se nao estiver , adicionar */
                 if(aux == NULL)
                     raiz = insere(raiz, palavra, sizePalavra);
+
+                /*Se estiver , incrementar sua frequencia */
                 else{
                     aux->freq++;
+
+                    /*Atualizar a frequencia maxima do texto */
                     if(aux->freq > maxFreq)
                         maxFreq = aux->freq;
+                free(palavra);
                 }
             }
             sizePalavra = 0;
         }
+        /*Caso nao seja o fim de uma palavra, incrementar o tamanho da palavra
+        e armazenar o caracter na palavra auxiliar */
         else
             auxPalavra[sizePalavra++] = c;
         i++;
     }
+
+    /*Criando o vetor de frequencias */
     celula ** frequencias = malloc(sizeof(celula *) * (maxFreq + 1));
 
+    /*Passando por cada node da arvore e adiconando a palavra ao vetor de frequencias.
+    Como eh inordem , as palavras serao colocadas em ordem alfabetica , 
+    como estao dispostas na arvore */
     inordem(raiz, frequencias);
-    while(k != 0){
-    printf("Digite um k (menor  ou igual a %d):", maxFreq + 1);
-    scanf("%d", &k);
-    imprimeListaRec(frequencias[k]);
 
+    while(k != 0){
+    /*Recebendo o k menor  ou igual a frequencia maxima */
+    printf("Digite um k (menor  ou igual a %d):", maxFreq );
+    scanf("%d", &k);
+
+    if(k > maxFreq)
+        printf("Nao ha frequencias maiores que %d\n", maxFreq);
+
+    /*Imprimindo as palavras com frequencia k */
+    else
+        imprimeListaRec(frequencias[k]);
+
+    }
+
+    for(i = 0; i < maxFreq + 1; i++){
+        free(frequencias[i]);
     }
 }
